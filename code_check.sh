a4b7462ab506aa50bfa938884cd9972b0c70b689
chromabox <chromarockjp@gmail.com>
2024-02-21 18:09:11 +0900

    fix check scripts

--- code_check.sh
+++ code_check.sh
@@ -5,8 +5,14 @@ echo "check code style and errors ...."
 cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cd ..
-cp build/compile_commands.json ./
-run-clang-tidy $PWD
-rm compile_commands.json
+clang-tidy $(find . -name "*.h" -or -name "*cpp") -p ./build --warnings-as-errors=*
+retval=$?
+if [ $retval -eq 0 ]
+then
+    echo "linter check success!!"
+else
+    echo "linter check fail!!"
+fi
 
 echo "done."
+exit $retval
