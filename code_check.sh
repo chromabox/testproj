970e74a7dc5e4f05153c15edd97a18574d1fb62f
chromabox <chromarockjp@gmail.com>
2024-02-22 14:23:49 +0900

    change linter to cpplint

--- code_check.sh
+++ code_check.sh
@@ -2,10 +2,7 @@
 
 echo "check code style and errors ...."
 
-cd build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-cd ..
-clang-tidy $(find . -name "*.h" -or -name "*cpp") -p ./build --warnings-as-errors=*
+cpplint --recursive src/
 retval=$?
 if [ $retval -eq 0 ]
 then
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 // this is no better code
 
-extern int sub(char *buf);
-
+// extern int sub(char *buf);
 int main(int argc, char **argv)
 {
 	const char *cpc;
 	char ppp[10];
+	int i;
 	const char *cpc2 = (const char*)cpc;
-	short b = argc;
+	int b = argc;
 	sub(ppp);
+	for(i = 0 ; i < 5 ; i++) {
+		i++;
+	}
 	return b;
 }
